a13b1ff481cce9c41c9c52125926368de0fde824
vibinkumar24 <vibinpvi2001@gmail.com>
2023-12-16 22:25:49 +0530

    add Savoy-E-commerce master

---
+++ src/savoy.js
@@ -0,0 +1,103 @@
+import React from "react";
+import './savoy.css';
+import Image1 from "./light3.png"
+import Image2 from "./face.jpg"
+import Image3 from "./twi.jpg"
+import Image4 from "./insta.jpg"
+import Icon from "./icon.png"
+
+function Savoy() {
+
+
+    return (
+
+
+        <>
+                <div id="firstdiv">
+                    <div id="shop">&nbsp;&nbsp;<a href="#">Shop</a>&nbsp;&nbsp;&nbsp; <a href="#">Elements</a></div>
+                    <div id="savoy"><img id="icon" src={Icon}/>&nbsp;SAVOY</div>
+                    <div id="pages"><a href="">Cart</a></div>
+                </div>
+               <div id="bye">
+                    <div id="hi2">Contemporary Pendant <br />Lighting<br /><br />Interior</div>
+                    <div id="hi3"><img src={Image1}/></div>
+                </div>
+                 <div id="seconddiv">
+                    <div><a href={"https://amzn.eu/d/1gdY7r5"}><img class="myimage" id="img1"
+                        src={"https://m.media-amazon.com/images/I/41WeC0IDIXL._SX300_SY300_QL70_FMwebp_.jpg"}/></a></div>
+                    <p id="lamp">In Dictum Sapien Ut Elit<br />&euro;58.00</p>
+                    <div><a href={"https://amzn.eu/d/93LRfmN"}><img class="myimage" id="img2"
+                        src={"https://m.media-amazon.com/images/I/41Br9QbKCcL._SX300_SY300_QL70_FMwebp_.jpg"}/></a>
+                    </div>
+                    <p id="flychar">Nisi Ut Aliqu<br />&euro;70.00</p>
+                    <div><a href={"https://amzn.eu/d/2Cro0Om"}><img class="myimage" id="img3"
+                        src={"https://m.media-amazon.com/images/I/41wnD3EUjwL._SY300_SX300_QL70_FMwebp_.jpg"}/></a>
+                    </div>
+                    <p id="kidchar">Ut Sollicitudin Gravida Tellu<br />&euro;55.00</p>
+                    <div><a href={"https://amzn.eu/d/epl0MyC"}><img class="myimage" id="img4"
+                        src={"https://m.media-amazon.com/images/I/51ET+9+bB1L._AC_UF1000,1000_QL80_FMwebp_.jpg"}/></a>
+                    </div>
+                    <p id="flower">Aut Odit Fugit<br />&euro;52.00</p>
+                    <div><a href={"https://amzn.eu/d/2lB1gjA"}><img class="myimage" id="img5"
+                        src={"https://m.media-amazon.com/images/I/41fi-d8J5gS._SX300_SY300_QL70_FMwebp_.jpg"}/></a></div>
+                    <p id="char">In purus Ipsum<br />&euro;60.00&nbsp;&nbsp;<del>&euro;65.00</del></p>
+                    <div><a href={"https://amzn.eu/d/1xbamm7"}><img class="myimage" id="img6"
+                        src={"https://m.media-amazon.com/images/I/81Oh6XIZBCL._UX679_.jpg"}/></a></div>
+                    <p id="bag">Viverra Posuere mattis<br />&euro;70.00</p>
+                    <div><a href={"https://amzn.eu/d/087u5sN"}><img class="myimage" id="img7"
+                        src={"https://m.media-amazon.com/images/I/31v5eFvuboL._SX300_SY300_QL70_FMwebp_.jpg"}/></a>
+                    </div>
+                    <p id="clean">Nunc Imperdiet Sed Erat Vel<br />&euro;28.00</p>
+                    <div><a href={"https://amzn.eu/d/0MA0yfT"}><img class="myimage" id="img8"
+                        src={"https://m.media-amazon.com/images/I/51a8s-eeeES._SX300_SY300_QL70_FMwebp_.jpg"}/></a></div>
+                    <p id="smartchar">Swatch Variation Image<br />&euro;69.00</p>
+
+                    <div><a href={"https://amzn.eu/d/4RIwIuL"}><img class="myimage" id="img9"
+                        src={"https://m.media-amazon.com/images/I/51azz3byCzL._SX300_SY300_QL70_FMwebp_.jpg"}/></a></div>
+                    <p id="paperbag">In Dictum Sapien Ut Elit<br />&euro;20.00</p>
+                    <div><a href={"https://amzn.eu/d/1QsFZzw"}><img class="myimage" id="img10"
+                        src={"https://m.media-amazon.com/images/I/31XJwz1QD2L._SY445_SX342_QL70_FMwebp_.jpg"}/></a>
+                    </div>
+                    <p id="light1">Am Sed Facilisis Ligula<br />&euro;75.00</p>
+                    <div><a href={"https://amzn.eu/d/fZVSd5N"}><img class="myimage" id="img11"
+                        src={"https://m.media-amazon.com/images/I/A1lLBjpm0sL._UY741_.jpg"}/></a>
+                    </div>
+                    <p id="bag2">Viverra Posuere mattis Eu<br />&euro;65.00</p>
+                    <div><a href={"https://amzn.eu/d/9jNArJT"}><img class="myimage" id="img12"
+                        src={"https://m.media-amazon.com/images/I/71ybbr7NihL._SY550_.jpg"}/></a></div>
+                    <p id="sitchar">Eu Ultrices Ex Tortor Quis Veli<br />&euro;52.00&nbsp;&nbsp;<del>&euro;65.00</del></p>
+                    <button>Next page...</button>
+                </div>
+                <div id="thirddiv">
+                    <div id="about">ABOUT STORE</div>
+                    <div id="contact">
+                        &#9750;&nbsp;&nbsp;Ranelagh Place, Liverpool, L3 5UL, England<br /><br />
+                        &#9743;&nbsp;&nbsp;9323827822<br /><br />
+                        &#9993;&nbsp;&nbsp;savoy@gmail.com<br /><br />
+                    </div>
+                    <div>
+                        <a href={"https://www.facebook.com/"}><img id="face" src={Image2} /></a>
+                        <a href={"https://twitter.com/login?lang=en"}><img id="twi" src={Image3} /></a>
+                        <a href={"https://www.instagram.com/"}><img id="insta" src={Image4} /></a>
+                    </div>
+                    <div id="lastdiv">
+                        <div id="fourthdiv">INFORMATION</div>
+                        <div id="guide">Shipping Guide<br /><br />
+                            Store<br /><br />
+                            Afiliates<br /><br />
+                            Customer Point Policy
+                        </div>
+                    </div>
+                    <div id="last">
+                        <div id="fifthdiv">CUSTOMER SERVICES</div>
+                        <div id="support">Product Support<br /><br />
+                            PC Setup & Support<br /><br />
+                            Services<br /><br />
+                            Community</div>
+                    </div>
+                </div> 
+            </>
+            )
+}
+
+            export default Savoy
